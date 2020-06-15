62922c6dec98e044be212cf0ca99e954e5046ced
minami.tanaka <yhntnsktk@gmail.com>
2020-06-15 08:36:50 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,10 +47,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'content' => 'required|max:255'
+            'status' =>'required|max;10',
+            'content' => 'required|max:255',
         ]);
         
         $task = new Task;
+        $task->status = $request->title;
         $task->content = $request->content;
         $task->save();
 
@@ -98,11 +100,13 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'content' =>'required|255',
+            'status' =>'required|max:10',
+            'content' =>'required|max:255',
         ]);
         
         $task =Task::findOrFail($id);
         
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
